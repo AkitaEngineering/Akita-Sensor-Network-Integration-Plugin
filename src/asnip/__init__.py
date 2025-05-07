aecc29b83fa686aa5fbc254ea4f30df8034df00b
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-05-07 11:52:05 -0400

    Create __init__.py

---
+++ src/asnip/__init__.py
@@ -0,0 +1,13 @@
+# This file makes the 'asnip' directory a Python package.
+# It can be empty.
+
+# Optionally, you could expose the main plugin class here if you wanted
+# to allow imports like 'from asnip import ASNIP', but for Meshtastic's
+# typical plugin loading mechanism (which often relies on filename and
+# a class named Plugin or the capitalized filename), this is usually
+# not strictly necessary.
+
+# from .asnip import ASNIP
+# __all__ = ['ASNIP']
+
+# For now, keeping it empty is standard and sufficient.
